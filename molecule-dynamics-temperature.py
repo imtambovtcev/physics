e24bb99e85fcbe999896eb8842106d86bfd0eea5
ivan <imtambovcev@gmail.com>
2021-05-25 16:49:06 +0300

    box_w

--- molecule-dynamics-temperature.py
+++ molecule-dynamics-temperature.py
@@ -11,6 +11,7 @@ from matplotlib.figure import Figure
 import matplotlib as mpl
 from scipy.spatial import distance_matrix
 import matplotlib.pyplot as plt
+from scipy.stats import maxwell
 
 class Combo(QtWidgets.QComboBox):
     def __init__(self, dict):
@@ -47,26 +48,29 @@ class State():
 
 class Model():
     def __init__(self,size=100):
-        self._size = size
         self.t=250.
         self.time=0.
-        self.data_save_rate=0.1
+        self.data_save_rate=0.01
+        self.data_save_rate_coeff = 1.
         self.last_data_update=0.
-        self.interaction= None#'6_12'
-        self.interaction_coef=-0.001
-        self.boundary_list={'free':'Периодические','box':'Ящик'}
+        self.reservoir_energy=0.
+        self.interaction_coef=0.001
+        self.interaction_dict = {'No':'Нет','hard_balls': 'Твердые сферы', '612':'Потенциал Леннарда-Джонса'}  # '6_12'
+        self.interaction = 'hard_balls'
+        self.boundary_dict={'free':'Периодические','box':'Коробка','box_w':'Коробка с прилипанием'}
         self.boundary='free'
         self.r=None
         self.v=None
         self.contact=None
         self.vabs=None
         self.kinetic_energy=None
-        self.set_on_lattice()
+        self.size = size
         self.updateInfo()
-        self.data_names={'time':'$t$','mean V':'$< V >$','mean V squared':'$<V^2>$','mean r':'$<\Delta r>$'}
-        self.data_captions = {'time': 'Время', 'mean V': 'Средняя скорость', 'mean V squared': 'Средний квадрат скорости','mean r': 'Среднее расстояние'}
+        self.data_names={'time':'$t$','mean V':'$< V >$','mean V squared':'$<V^2>$','mean r':'$<\Delta r>$','V dist':'$V$','r dist':'$\Delta r$'}
+        self.data_captions = {'time': 'Время', 'mean V': 'Средняя скорость', 'mean V squared': 'Средний квадрат скорости',
+                              'mean r': 'Среднее расстояние','V dist':'Распределение скоростей','r dist':'Распределение расстояний'}
         self.data_values = {'time': np.array([]), 'mean V': np.array([]),
-                          'mean V squared': np.array([]),'mean r': np.array([])}
+                          'mean V squared': np.array([]),'mean r': np.array([]),'V dist': np.array([]),'r dist': np.array([])}
         self.data='mean V'
 
     @property
@@ -76,10 +80,11 @@ class Model():
     @size.setter
     def size(self,value):
         value=int(value)
-        if value > self._size:
-            self._size=value
-        elif value<self._size:
-            self._size = value
+        self._size = value
+        self.set_on_lattice()
+        print(f'{self.size = }')
+
+
 
     def set_on_lattice(self):
         nx=ny=int(np.ceil(np.sqrt(self.size)))
@@ -105,12 +110,48 @@ class Model():
         self.contact = self.r // 1
         if self.boundary == 'box':
             self.v-=2*self.v*np.abs(self.contact)
+        elif self.boundary == 'box_w':
+            is_out=(np.sum((self.contact!=0),axis=1))!=0
+            is_out_n=np.sum(is_out)
+            if is_out_n>0:
+                #print(f'{np.mean(np.abs(self.v)) = }')
+                #print(f'{is_out = }\n{is_out_n = }')
+                #print(f'{np.linalg.norm(self.v[is_out,:],axis=1).shape = }')
+                #print(f'{np.linalg.norm(self.v[is_out,:],axis=1) = }\n{np.linalg.norm(self.v[is_out,:],axis=1)*np.linalg.norm(self.v[is_out,:],axis=1) = }')
+                energy_out=np.sum(np.linalg.norm(self.v[is_out,:],axis=1)*np.linalg.norm(self.v[is_out,:],axis=1))+self.reservoir_energy
+                #print(f'{energy_out/(is_out_n+1) = }')
+                #abs_v=1+np.random.standard_normal([is_out_n+1])
+                abs_v=maxwell.rvs(size=is_out_n+1)
+                abs_v*=np.sqrt(energy_out/np.sum(abs_v*abs_v))
+                #print(f'{abs_v*abs_v = }')
+                self.reservoir_energy=abs_v[-1]*abs_v[-1]
+                abs_v=abs_v[:-1]
+                #print(f'{abs_v = }')
+                #print(f'{self.reservoir_energy = }')
+                ang=2*np.pi*np.random.rand(is_out_n)
+                new_velocities=np.array([np.multiply(abs_v,np.cos(ang)),np.multiply(abs_v,np.sin(ang))]).T
+                #print(f'{new_velocities = }')
+                #print(f'{self.contact[is_out] = }\n{self.contact[is_out] != 0 = }\n{self.contact[self.contact!=0] = }')
+                new_velocities[self.contact[is_out] != 0] = -self.contact[self.contact!=0]*np.abs(new_velocities[self.contact[is_out] != 0])
+                #print(f'{self.v[is_out,:].shape = }')
+                self.v[is_out,:]=new_velocities
+                #self.v[self.contact!=0]=-self.contact*self.v[self.contact!=0]
+                #print(f'{new_velocities = }\n{energy_rand = }')
+            #self.v -= 2 * self.v * np.abs(self.contact)
         else:
             self.r-=self.contact
 
     def run(self, dt):
         self.time+=dt
-        if self.interaction == '6_12':
+        if self.interaction == 'hard_balls':
+            dm=distance_matrix(self.r,self.r)
+            print(f'{dm = }\n{dm.shape = }')
+            ineraction_distance=0.05
+            print(np.mean(f'{dm.mean() = }'))
+            if np.any(dm<ineraction_distance):
+                print(f'{np.sum(dm<ineraction_distance) = }')
+            a = 0.
+        elif self.interaction == '612':
             all_r = np.concatenate([self.r, self.r + np.array([0, 1]), self.r + np.array([1, 0]),
                                     self.r - np.array([0, 1]), self.r - np.array([1, 0]),
                                     self.r + np.array([1, 1]), self.r + np.array([1, -1]),
@@ -146,7 +187,7 @@ class Model():
         self.vabs=np.linalg.norm(self.v,axis=1)
         k=np.linalg.norm(self.vabs)
         self.kinetic_energy=k*k
-        if self.interaction != '6_12':
+        if not self.interaction == '612':
             all_r = np.concatenate([self.r, self.r + np.array([0, 1]), self.r + np.array([1, 0]),
                                     self.r - np.array([0, 1]), self.r - np.array([1, 0]),
                                     self.r + np.array([1, 1]), self.r + np.array([1, -1]),
@@ -157,13 +198,27 @@ class Model():
 
 
     def updateData(self):
-        if self.time-self.last_data_update>=self.data_save_rate:
+        if self.time-self.last_data_update>=self.data_save_rate*self.data_save_rate_coeff:
             self.data_values['time']=np.append(self.data_values['time'],self.time)
             self.data_values['mean V']=np.append(self.data_values['mean V'],np.mean(self.vabs))
             self.data_values['mean V squared'] = np.append(self.data_values['mean V squared'], np.mean(np.multiply(self.vabs,self.vabs)))
-            self.data_values['mean r'] = np.append(self.data_values['mean r'],
-                                                           np.mean(self.d))
+            self.data_values['mean r'] = np.append(self.data_values['mean r'], np.mean(self.d))
+            self.data_values['V dist'] = np.copy(self.vabs)
+            self.data_values['r dist'] = np.copy(self.d).reshape(-1)
             self.last_data_update=self.time
+
+            if len(self.data_values['time'])>200:
+                for key in self.data_values.keys():
+                    self.data_values[key] = self.data_values[key][::2]
+                self.data_save_rate_coeff*=2
+                #print(f'{self.data_save_rate_coeff = }\n{self.data_save_rate*self.data_save_rate_coeff = }')
+
+    def clearData(self):
+        self.data_values = {'time': np.array([]), 'mean V': np.array([]),
+                            'mean V squared': np.array([]), 'mean r': np.array([]), 'V dist': np.array([]),
+                            'r dist': np.array([])}
+        self.data_save_rate_coeff=1.
+        self.last_data_update=0.
 
 class RenderWidget(QtWidgets.QWidget):
     def __init__(self, *vargs, model=None,state=None, **kwargs):
@@ -233,19 +288,21 @@ class RenderWidget(QtWidgets.QWidget):
             #print('Plot updated!')
 
     def timePlotUpdate(self):
-        if not self.state.pause or self.state.update_plot_request:
+        if (not self.state.pause) or self.state.update_plot_request:
             file=None if not self.state.record else self.save_directory+'{}'.format(self.state.record_n)
             self.updatePlot(file)
 
 
-
-######################################################################################################
 
 class ParamWidget(QtWidgets.QWidget):
     def __init__(self, *vargs, model=None,state=None, **kwargs):
         super().__init__(*vargs, **kwargs)
         self.model = model
         self.state=state
+
+        self.isfree = False
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.updateState)
 
         self.randomWidget = QtWidgets.QPushButton("Случайное состояние")
         self.randomWidget.clicked.connect(self.onRandom)
@@ -256,33 +313,50 @@ class ParamWidget(QtWidgets.QWidget):
         self.resetWidget = QtWidgets.QPushButton("Перезапустить")
         self.resetWidget.clicked.connect(self.onReset)
 
+        self.nWidget = QtWidgets.QSlider(orientation=QtCore.Qt.Horizontal)
+        self.nWidget.setMinimum(1)
+        self.nWidget.setMaximum(500)
+        self.nWidget.valueChanged.connect(self.onnValue)
+        self.nWidget.setFixedWidth(200)
         self.tWidget = QtWidgets.QSlider(orientation=QtCore.Qt.Horizontal)
         self.tWidget.setMinimum(0)
         self.tWidget.setMaximum(500)
         self.tWidget.valueChanged.connect(self.ontValue)
         self.tWidget.setFixedWidth(200)
+        self.sWidget = QtWidgets.QSlider(orientation=QtCore.Qt.Horizontal)
+        self.sWidget.setMinimum(0)
+        self.sWidget.setMaximum(20)
+        self.sWidget.valueChanged.connect(self.onsValue)
+        self.sWidget.setFixedWidth(200)
+
         self.pauseButton = QtWidgets.QPushButton('Запуск')
         self.pauseButton.clicked.connect(self.onPause)
 
-        self.bWidget=Combo(self.model.boundary_list)
+        self.bWidget=Combo(self.model.boundary_dict)
         self.bWidget.currentIndexChanged.connect(self.onbValue)
+        self.iWidget = Combo(self.model.interaction_dict)
+        self.iWidget.currentIndexChanged.connect(self.oniValue)
 
         vbox = QtWidgets.QVBoxLayout()
         vbox.addWidget(self.resetWidget)
         vbox.addWidget(self.fmWidget)
         vbox.addWidget(self.randomWidget)
+        vbox.addWidget(QtWidgets.QLabel("Количество частиц"))
+        vbox.addWidget(self.nWidget)
         vbox.addWidget(QtWidgets.QLabel("Температура"))
         vbox.addWidget(self.tWidget)
+        vbox.addWidget(QtWidgets.QLabel("Граничные условия"))
         vbox.addWidget(self.bWidget)
+        vbox.addWidget(QtWidgets.QLabel("Взаимодействие"))
+        vbox.addWidget(self.iWidget)
+        vbox.addWidget(QtWidgets.QLabel("Скорость воспроизведения"))
+        vbox.addWidget(self.sWidget)
         vbox.addWidget(self.pauseButton)
         vbox.addStretch(1)
         self.setLayout(vbox)
         self.reset_parameters()
         self.show()
-
         self.isfree = True
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.updateState)
 
     def updateState(self):
         if self.isfree:
@@ -296,35 +370,72 @@ class ParamWidget(QtWidgets.QWidget):
 
     def reset_parameters(self):
         self.model.time=0.
+        self.n=self.model.size
         self.t=250
+        self.s = 0
+        self.pause=True
 
     def onReset(self, value):
         self.reset_parameters()
+        self.model.clearData()
 
     def onFM(self, value):
         self.model.set_on_lattice()
         self.state.update_request=True
+        self.state.update_plot_request = True
 
     def onRandom(self, value):
         self.model.set_rand()
         self.state.update_request = True
+        self.state.update_plot_request = True
+
+    def onnValue(self, value):
+        self.n = value
+        self.state.update_request = True
+        self.state.update_plot_request = True
 
     def ontValue(self, value):
-        self.model.t = value
+        self.t = value
+
+    def onsValue(self, value):
+        self.s = value
 
     def onbValue(self):
         self.model.boundary=self.bWidget.currentElement()
         print(f'{self.model.boundary = }')
 
+    def oniValue(self):
+        self.model.interaction=self.iWidget.currentElement()
+        print(f'{self.model.interaction = }')
+
     def onPause(self):
+        self.pause=not(self.state.pause)
+
+    @property
+    def pause(self):
+        return self.state.pause
+
+    @pause.setter
+    def pause(self,value):
+        self.state.pause=value
         if self.state.pause:
-            self.timer.start(int(1000 / 10))
-            self.state.pause = False
-            self.pauseButton.setText('Пауза')
-        else:
             self.timer.stop()
             self.state.pause = True
             self.pauseButton.setText('Запуск')
+        else:
+            self.timer.start(int(self.s)*10)
+            self.state.pause = False
+            self.pauseButton.setText('Пауза')
+
+    @property
+    def n(self):
+        return self.model.size
+
+    @n.setter
+    def n(self, value):
+        self.model.size = value
+        self.nWidget.setValue(int(self.n))
+        print(f'{self.n = }')
 
     @property
     def t(self):
@@ -334,6 +445,18 @@ class ParamWidget(QtWidgets.QWidget):
     def t(self, value):
         self.model.t = value
         self.tWidget.setValue(int(self.t))
+        print(f'{self.t = }')
+
+    @property
+    def s(self):
+        return self._s
+
+    @s.setter
+    def s(self, value):
+        self._s = value
+        self.sWidget.setValue(int(self._s))
+        self.timer.setInterval(int(self._s)*10)
+        print(f'{self.s = }')
 
     @property
     def boundary(self):
@@ -343,6 +466,7 @@ class ParamWidget(QtWidgets.QWidget):
     def boundary(self, value):
         self.model.boundary = value
         self.tWidget.setValue(value)
+        print(f'{self.model.boundary = }')
 
 class GraphWindow(QtWidgets.QWidget):
     def __init__(self, parent,model=None,state=None):
@@ -383,9 +507,18 @@ class GraphWindow(QtWidgets.QWidget):
         if self.isfree:
             self.isfree=False
             self.ax.clear()
-            self.ax.plot(self.model.data_values['time'],self.model.data_values[self.current_plot])
-            self.ax.set_xlabel('$t$')
-            self.ax.set_ylabel(self.model.data_names[self.current_plot])
+            if self.current_plot == 'V dist':
+                self.ax.hist(self.model.data_values[self.current_plot],bins=20)
+                self.ax.set_xlabel('$V$')
+                self.ax.set_ylabel('$Вероятность$')
+            elif self.current_plot == 'r dist':
+                self.ax.hist(self.model.data_values[self.current_plot],bins=20)
+                self.ax.set_xlabel('$r$')
+                self.ax.set_ylabel('$Вероятность$')
+            else:
+                self.ax.plot(self.model.data_values['time'],self.model.data_values[self.current_plot])
+                self.ax.set_xlabel('$t$')
+                self.ax.set_ylabel(self.model.data_names[self.current_plot])
             self.canvas.draw()
             self.isfree = True
             self.state.update_plot_request=False
